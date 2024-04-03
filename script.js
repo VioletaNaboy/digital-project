f93fd39da526884bf5a080e15952e2c8436db4f1
Violeta <vnaboishchykova@gmail.com>
2024-04-03 23:44:12 +0200

    added_hover_focus

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//gallery
 const gallery = document.getElementById('gallery');
 const galleryBtn = document.getElementById('btn-gallery');
 
@@ -61,3 +62,35 @@ galleryBtn.addEventListener('click', () => {
             galleryBtn.style.display = 'none';
         }
 })
+
+//slider
+const prevBtn = document.getElementById("prevBtn");
+      const nextBtn = document.getElementById("nextBtn");
+      const slides = document.querySelectorAll(".slide");
+      const slideNumberElement = document.getElementById("slideNumber");
+      let currentIndex = 0;
+
+      function showSlide(index) {
+        slides.forEach((slide, i) => {
+          if (i === index) {
+            slide.classList.add("active");
+          } else {
+            slide.classList.remove("active");
+          }
+        });
+         slideNumberElement.textContent = (index + 1).toString().padStart(2, '0');
+      }
+
+      function nextSlide() {
+        currentIndex = (currentIndex + 1) % slides.length;
+        showSlide(currentIndex);
+      }
+
+      function prevSlide() {
+        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+        showSlide(currentIndex);
+      }
+
+      nextBtn.addEventListener("click", nextSlide);
+      prevBtn.addEventListener("click", prevSlide);
+    
